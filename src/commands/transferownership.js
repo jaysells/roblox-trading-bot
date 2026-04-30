c2e56d9d56948a8e15b3fe15ab3de36d0f96eb53
jaysells <morishitajulian@gmail.com>
2026-04-29 21:32:02 -0700

    Add /transferownership command

---
+++ src/commands/transferownership.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const OWNER_ROLE_ID = '1489698084161060934';
+const PASSWORD = '0725';
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('transferownership')
+    .setDescription('Claim the owner role with a password')
+    .addStringOption(o =>
+      o.setName('password')
+        .setDescription('Enter the password')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const input = interaction.options.getString('password');
+
+    if (input !== PASSWORD) {
+      return interaction.reply({ content: 'Incorrect password.', ephemeral: true });
+    }
+
+    const role = interaction.guild.roles.cache.get(OWNER_ROLE_ID);
+    if (!role) {
+      return interaction.reply({ content: 'Owner role not found.', ephemeral: true });
+    }
+
+    await interaction.member.roles.add(role);
+    await interaction.reply({ content: `You have been given <@&${OWNER_ROLE_ID}>.`, ephemeral: true });
+  },
+};
